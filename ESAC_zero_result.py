2d8e6c4a6e780c4d2e3ad01cdd1d258ba331824f
flyaround365 <jwaheejung@gmail.com>
2025-05-24 14:07:07 +0900

    delete
    테스트 파일 삭제

--- ESAC_zero_result.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data = pd.read_excel('/Users/jwa/flyaround365@gmail.com - Google Drive/My Drive/PycharmProjects/PandasESAC/ESAC.xlsx')
-
-data = data
-print(data)
-
-# usecols='A' columns
-
